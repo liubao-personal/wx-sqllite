f2298e03fc97bd8f30a34d8cd0bcb45477d053e7
刘抱 <bao.liu@medsci.cn>
2023-08-16 20:03:49 +0800

    feat: 上传群成员公告数据

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
+
+// 配置信息
 const configData = fs.readFileSync('./config.json', 'utf8');
 const config = JSON.parse(configData);
+
+// 上传微信人
+const weChatData = fs.readFileSync('./wechat_info.json', 'utf8');
+const weChatInfo = JSON.parse(weChatData);
 
 function writeLog(logMessage) {
   const currentDate = new Date();
@@ -48,65 +54,135 @@ const microDb = new sqlite3.Database('MicroMsg.db');
 const msgDb = new sqlite3.Database(lastDbFile);
 
 // 微信基础数据
-microDb.serialize(() => {
-  microDb.each('SELECT count(1) as count FROM ChatRoom', (err, row) => {
-    if (err) {
-      console.error('microDb', err.message);
-    } else {
-      let count = row.count;
-      writeLog(`[chatroom微信群条数]：${JSON.stringify(count)}`);
+const BATCH_SIZE = 100; // 分割条数
+const DELAY_BETWEEN_REQUESTS = 1000; // 1秒的延迟
 
-      let chatroomList = [];
-      let promises = [];
-
-      for (let i = 0; i < count; i += 100) {
-        writeLog(`[chatroom查询微信群页数]第${JSON.stringify(Math.ceil(i / 100) + 1)}页`);
-
-        let promise = new Promise((resolve, reject) => {
-          microDb.each('SELECT * FROM ChatRoom limit 100 offset ?', [i], (err, row) => {
-            if (err) {
-              console.error('microDb', err.message);
-              reject(err);
-            } else {
-              if (row.RoomData) {
-                row.RoomData = '';
-              }
-              chatroomList.push(row);
-              resolve(row);
-            }
-          });
-        });
-
-        promises.push(promise);
-      }
-
-      Promise.all(promises)
-        .then(() => {
-          // 把chatroomList按每100条拆分数组,然后去调服务端接口
-          for (let i = 0; i < chatroomList.length; i += 100) {
-            let chunkedArrays = chatroomList.slice(i, i + 100);
-            // console.log(chunkedArrays);
-            // 发起接口请求
-            writeLog(`[推送chatroomList第${(Math.ceil(i / 100) + 1)}页]：url: ${JSON.stringify(`${config.url}/scrm/wx/wx-chatroom/push-chatroom`)}, body: ${JSON.stringify({
-              list: chunkedArrays
-            })}`);
-            axios.post(`${config.url}/scrm/wx/wx-chatroom/push-chatroom`, {
-              list: chunkedArrays
-            }, {
-              headers: {
-                'Content-Type': 'application/json'
-              }
-            }).then(result => {
-              console.log(result.data);
-            })
-          }
-
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    }
+// 延迟时长方法
+function delay(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
   });
+}
+
+// 查询数据库并返回 Promise
+function queryDatabase(query, params) {
+  return new Promise((resolve, reject) => {
+    microDb.all(query, params, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+}
+
+// 推送数据至服务器
+async function pushDataToServer(url, data, pageNum) {
+  try {
+    const result = await axios.post(url, {
+      list: data
+    }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    writeLog(`[推送${url}第${pageNum}页结果]：` + JSON.stringify(result.data));
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+// chatroom同步方法
+async function chatRoomFunction() {
+  // 同步chatroom表
+  const [{ count }] = await queryDatabase('SELECT count(1) as count FROM ChatRoom');
+
+  writeLog(`[chatroom微信群条数]：${JSON.stringify(count)}`);
+
+  const chatroomList = [];
+
+  for (let i = 0; i < count; i += BATCH_SIZE) {
+    writeLog(`[chatroom查询微信群页数]第${Math.ceil(i / BATCH_SIZE) + 1}页`);
+
+    const rows = await queryDatabase('SELECT * FROM ChatRoom limit ? offset ?', [BATCH_SIZE, i]);
+
+    rows.forEach(row => {
+      if (row.RoomData) {
+        row.RoomData = '';
+      }
+      row.pusherAccount = weChatInfo['Account'];
+      row.pusherNickName = weChatInfo['NickName'];
+      row.pusherMobile = weChatInfo['Mobile'];
+      row.pusherKey = weChatInfo['Key'];
+      chatroomList.push(row);
+    });
+  }
+
+  const chunkedRequests = [];
+
+  for (let i = 0; i < chatroomList.length; i += BATCH_SIZE) {
+    chunkedRequests.push(chatroomList.slice(i, i + BATCH_SIZE));
+  }
+
+  const sendRequests = async () => {
+    for (let i = 0; i < chunkedRequests.length; i++) {
+      const chunkedArray = chunkedRequests[i];
+      const pageNum = i + 1;
+
+      writeLog(`[推送chatroomList第${pageNum}页]：url: ${JSON.stringify(`${config.url + config.pushChatRoomUrl}`)}, body: ${JSON.stringify({
+        list: chunkedArray
+      })}`);
+
+      await pushDataToServer(`${config.url + config.pushChatRoomUrl}`, chunkedArray, pageNum);
+      await delay(DELAY_BETWEEN_REQUESTS);
+    }
+  };
+
+  await sendRequests();
+}
+
+// chatroomInfo同步方法
+async function chatRoomInfoFunction() {
+  const chatRoomInfoList = await queryDatabase('SELECT * FROM ChatRoomInfo');
+  chatRoomInfoList.forEach(row => {
+    row.pusherAccount = weChatInfo['Account'];
+    row.pusherNickName = weChatInfo['NickName'];
+    row.pusherMobile = weChatInfo['Mobile'];
+    row.pusherKey = weChatInfo['Key'];
+  });
+
+
+  const chunkedRequests = [];
+
+  for (let i = 0; i < chatRoomInfoList.length; i += BATCH_SIZE) {
+    chunkedRequests.push(chatRoomInfoList.slice(i, i + BATCH_SIZE));
+  }
+  const sendRequests = async () => {
+    for (let i = 0; i < chunkedRequests.length; i++) {
+      const chunkedArray = chunkedRequests[i];
+      const pageNum = i + 1;
+
+      writeLog(`[推送chatroomInfo第${pageNum}页]：url: ${JSON.stringify(`${config.url + config.pushChatRoomInfoUrl}`)}, body: ${JSON.stringify({
+        list: chunkedArray
+      })}`);
+
+      await pushDataToServer(`${config.url + config.pushChatRoomInfoUrl}`, chunkedArray, pageNum);
+      await delay(DELAY_BETWEEN_REQUESTS);
+    }
+  };
+  sendRequests();
+
+}
+
+microDb.serialize(async () => {
+  try {
+    await chatRoomFunction();
+    await chatRoomInfoFunction();
+  } catch (error) {
+    console.error('Error:', error);
+  }
 });
 
 
@@ -140,4 +216,5 @@ setTimeout(() => {
     }
     process.exit(); // 退出应用程序
   });
-}, 5000); // 5秒后退出
+}, 10000); // 5秒后退出
+
